35e980294922dd1012fc2df8abe024935e806c3b
Fawwad Abdul Ahad <fawwadabdulahad500@gmail.com>
2026-08-30 18:42:08 +0500

    chainge in db

--- db.py
+++ db.py
@@ -5,16 +5,25 @@ from langchain_community.vectorstores import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
+def create_db():
+    data = PyPDFLoader(r'C:\Users\fawwa\Desktop\Rag Project\React Native & JavaScript - 400 Practice Questions.pdf')
+    docs = data.load()
 
-data = PyPDFLoader(r'C:\Users\fawwa\Desktop\Rag Project\React Native & JavaScript - 400 Practice Questions.pdf')
-docs = data.load()
-
-splitter  = RecursiveCharacterTextSplitter(
-    separators='',
-    chunk_size = 500,
-    chunk_overlap = 100
-)
+    splitter  = RecursiveCharacterTextSplitter(
+        separators='',
+        chunk_size = 500,
+        chunk_overlap = 100
+    )
 
 
-chunks = splitter.split_documents(docs)
-print(len(chunks))
+    chunks = splitter.split_documents(docs)
+    print(len(chunks))
+
+    embedding_model = MistralAIEmbeddings()
+
+    vectorStore = Chroma.from_documents(
+        documents = chunks,
+        embedding = embedding_model,
+        persist_directory='chroma_db'
+    )
+
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain_core.runnables import RunnableSequence
 from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
-
+create_db()
 model = ChatMistralAI(
     model = "mistral-small-2506"
 )
@@ -21,20 +21,5 @@ template = ChatPromptTemplate.from_messages(
 
 parser = StrOutputParser()
 
-data = PyPDFLoader(r'C:\Users\fawwa\Desktop\Rag Project\React Native & JavaScript - 400 Practice Questions.pdf')
-docs = data.load()
-
-splitter  = RecursiveCharacterTextSplitter(
-    separators='',
-    chunk_size = 500,
-    chunk_overlap = 100
-)
-
-
-chunks = splitter.split_documents(docs)
-print(len(chunks))
-
 chain = template | model | parser
-
-result = chain.invoke({"data" : docs})
 # print(result)
